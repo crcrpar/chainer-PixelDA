dd4e5e5d835c399fdf98ef02fa62f47c3735cd46
Naoto Inoue <inoue@hal.t.u-tokyo.ac.jp>
2017-07-23 22:35:46 +0900

    write basic blocks

--- net.py
+++ net.py
@@ -1,6 +1,7 @@
 import chainer
 import chainer.functions as F
 import chainer.links as L
+from chainer import cuda
 
 
 class DigitClassifier(chainer.Chain):
@@ -19,3 +20,93 @@ class DigitClassifier(chainer.Chain):
         h = F.relu(self.fc1(h))
         h = F.relu(self.fc2(h))
         return self.fc3(h)
+
+
+class GenResBlock(chainer.Chain):
+    def __init__(self, out_channel, w):
+        super(GenResBlock, self).__init__()
+        with self.init_scope():
+            self.conv1 = L.Convolution2D(None, out_channel, 3, 1, initialW=w)
+            self.conv2 = L.Convolution2D(None, out_channel, 3, 1, initialW=w)
+            self.bn1 = L.BatchNormalization(out_channel, use_gamma=False)
+            self.bn2 = L.BatchNormalization(out_channel, use_gamma=False)
+
+    def __call__(self, x):
+        h = F.relu(self.bn1(self.conv1(x)))
+        h = x + self.bn1(self.conv1(h))
+        return h
+
+
+class Generator(chainer.Chain):
+    def __init__(self, n_resblock=6, ch=64, wscale=0.02):
+        super(Generator, self).__init__()
+        self.ch = ch
+        self.n_resblock = n_resblock
+        w = chainer.initializers.Normal(wscale)
+        with self.init_scope():
+            # TODO ここの合流がマジで謎
+            self.conv1 = None
+            self.block1 = GenResBlock(ch, w)
+            self.block2 = GenResBlock(ch, w)
+            self.block3 = GenResBlock(ch, w)
+            self.block4 = GenResBlock(ch, w)
+            self.block5 = GenResBlock(ch, w)
+            self.block6 = GenResBlock(ch, w)
+            self.conv2 = L.Convolution2D(None, 3, 3, 1, initialW=w)
+
+    def __call__(self, z):
+        h = None
+        h = F.relu(self.conv1(h))
+        h = self.block1(h)
+        h = self.block2(h)
+        h = self.block3(h)
+        h = self.block4(h)
+        h = self.block5(h)
+        h = self.block6(h)
+        return F.tanh(self.conv2(h))
+
+
+def add_noise(h, sigma=0.2):
+    xp = cuda.get_array_module(h.data)
+    if chainer.config.train:
+        return h + sigma * xp.random.randn(*h.shape)
+    else:
+        return h
+
+
+class DisBlock(chainer.Chain):
+    def __init__(self, out_channel, initialW):
+        super(DisBlock, self).__init__()
+        with self.init_scope():
+            self.conv = L.Convolution2D(None, out_channel, 3, 2,
+                                        initialW=initialW)
+            self.bn = L.BatchNormalization(out_channel, use_gamma=False)
+
+    def __call__(self, x):
+        # TODO check if the position of add_noise is OK
+        return F.dropout(F.leaky_relu(add_noise(self.bn(self.conv(x)))), 0.9)
+
+
+class Discriminator(chainer.Chain):
+    def __init__(self, ch=1024, wscale=0.02):
+        super(Discriminator, self).__init__()
+        # ch = 512 in mnist-m experiment
+        self.ch = ch
+        w = chainer.initializers.Normal(wscale)
+        with self.init_scope():
+            self.conv = L.Convolution2D(None, 64, 3, 1, initialW=w)
+            self.bn = L.BatchNormalization(64, use_gamma=False)
+            self.block1 = DisBlock(ch // 8, initialW=w)
+            self.block2 = DisBlock(ch // 4, initialW=w)
+            self.block3 = DisBlock(ch // 2, initialW=w)
+            self.block4 = DisBlock(ch, initialW=w)
+            self.fc = L.Linear(None, 1, initialW=w)
+
+    def __call__(self, x):
+        h = add_noise(x)
+        h = F.dropout(F.leaky_relu(add_noise(self.bn(self.conv(h)))), 0.9)
+        h = self.block1(h)
+        h = self.block2(h)
+        h = self.block3(h)
+        h = self.block4(h)
+        return self.fc(h)
